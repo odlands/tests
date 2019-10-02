0f76fafbf9b8fce2a3bd1ef8dd3bfc6e892d4bf0
Alex Worthy <alex.w.worthy@gmail.com>
2019-10-02 14:23:44 -0600

    Updateto previous project state + Database support

---
+++ DbTest/Assets/Scripts/SinglePlayButt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SinglePlayButt : MonoBehaviour
+{
+    public void SinglePlayGame()
+    {
+        SceneManager.LoadScene("Level 1 Single");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
